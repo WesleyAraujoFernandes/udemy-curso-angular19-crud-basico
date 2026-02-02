2097bc9f5695d049261cb6a5c8aaf31d1fce3395
knowledgesolutionstecnology <knowledgesolutionstecnology@gmail.com>
2026-02-02 17:39:06 -0300

    salvando e consultando no localstorage

--- src/app/consulta/consulta.component.ts
+++ src/app/consulta/consulta.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-consulta',
-  imports: [],
+  imports: [FlexLayoutModule, MatCardModule, MatFormFieldModule, MatInputModule, FormsModule, MatIconModule, MatButtonModule],
   templateUrl: './consulta.component.html',
   styleUrl: './consulta.component.scss'
 })
