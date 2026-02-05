52b44b74353ce78955099be086b1738d39d22786
knowledgesolutionstecnology <knowledgesolutionstecnology@gmail.com>
2026-02-05 08:53:22 -0300

    implementação do combo de UF

--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -7,17 +7,19 @@ import { FormsModule } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatSelectModule } from '@angular/material/select';
 import { Cliente } from './cliente';
 import { ClienteService } from '../cliente.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { NgxMaskDirective, provideNgxMask } from 'ngx-mask';
 import { BrasilapiService } from '../brasilapi.service';
 import { Estado, Municipio } from '../brasilapi.models';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-cadastro',
-  imports: [FlexLayoutModule, MatCardModule, FormsModule, MatFormFieldModule,
-    MatInputModule, MatIconModule, MatButtonModule, NgxMaskDirective],
+  imports: [FlexLayoutModule, MatCardModule, MatSelectModule, FormsModule, MatFormFieldModule,
+    MatInputModule, MatIconModule, MatButtonModule, NgxMaskDirective, CommonModule],
   providers: [
     provideNgxMask()
   ],
@@ -55,7 +57,7 @@ export class CadastroComponent implements OnInit {
 
   carregarUFs() {
     this.brasilApiService.listarUFs().subscribe({
-      next: listaEstados => console.log("lista estados", listaEstados),
+      next: listaEstados => this.estados = listaEstados,
       error: erro => console.log("ocorreu um erro: ", erro)
     });
   }
